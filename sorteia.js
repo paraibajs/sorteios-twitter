39cc0e6a82cacc63af22c15cd60e70702e356b03
Sergio Vilar <sergiovilar.r@gmail.com>
2015-10-03 17:54:20 -0300

    Número de sorteados como argumento opcional

--- sorteia.js
+++ sorteia.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var participantes = require('./data.json');
-var numSorteados = process.argv[2];
+var numSorteados = process.argv[2] || 1;
 var total = participantes.length - 1;
 
 request('http://www.random.org/integers/?num='+numSorteados+'&min=0&max='+total+'&col=1&base=10&format=plain&rnd=new', function (error, response, body) {
